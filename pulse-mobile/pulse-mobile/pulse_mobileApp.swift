545c1378f02f735542abd75dccd939a826daa17d
AJ Johnson <54604582+AjJ132@users.noreply.github.com>
2025-08-08 06:33:11 -0400

    refactor

---
+++ pulse-mobile/pulse-mobile/pulse_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_mobileApp.swift
+//  pulse-mobile
+//
+//  Created by Alan Johnson on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct pulse_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pulse-mobile/pulse-mobileTests/pulse_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_mobileTests.swift
+//  pulse-mobileTests
+//
+//  Created by Alan Johnson on 8/8/25.
+//
+
+import Testing
+@testable import pulse_mobile
+
+struct pulse_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
